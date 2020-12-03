f2c0e6c4b1fabaa163a76cbcd35ac12a2ff7e938
iamPraffulVerma <praffulrakeshverma2001@gmail.com>
2020-12-03 18:04:27 +0530

    Navbar syntax completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router , Switch , Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import Favourite from "./pages/Favourite";
+import Login from "./pages/Login";
+import Recommendation from "./pages/Recommendation";
+import Support from "./pages/Support";
 
 function App() {
   return (
@@ -8,7 +14,11 @@ function App() {
      <Router>
        <Navbar />
        <Switch>
-         <Route path='/' />
+         <Route path='/' exact component={Home} />
+         <Route path='/login' component={Login} />
+         <Route path='/recommendation' component={Recommendation} />
+         <Route path='/favourite' component={Favourite} />
+         <Route path='/support' component={Support} />
        </Switch>
      </Router> 
     </>
---
+++ src/pages/Support.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Support() {
+    return(
+        <div className='Support'>
+             <h1>support</h1>
+        </div>
+    )    
+}
+
+export default Support;
